580b994ad918439afd699adc7844a1e3000dcd69
jviitsm <jvictorgmodesto@gmail.com>
2017-05-31 02:17:51 -0300

    exceptions

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/myeasyparking/Cadastro.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/myeasyparking/Cadastro.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by root on 29/05/17.
@@ -22,7 +23,7 @@ public class Cadastro extends AppCompatActivity {
 
         final EditText edtUser = (EditText) findViewById(R.id.edtLogin);
         final EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
-        EditText edtRepetirSenha = (EditText) findViewById(R.id.edtSenhaRepetir);
+        final EditText edtRepetirSenha = (EditText) findViewById(R.id.edtSenhaRepetir);
         final EditText edtEstacionamento = (EditText) findViewById(R.id.edtEstacionamento);
 
         Button btCadastrar = (Button) findViewById(R.id.btCadastrar);
@@ -31,21 +32,29 @@ public class Cadastro extends AppCompatActivity {
         btCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EstacionamentoDAO esta = new EstacionamentoDAO(Cadastro.this);
-                if(estacionamento == null){
-                    estacionamento = new Estacionamento();
+                if(edtUser.getText().toString().isEmpty() || edtSenha.getText().toString().isEmpty() ||
+                        edtRepetirSenha.getText().toString().isEmpty() || edtEstacionamento.getText().toString().isEmpty()){
+                    Toast.makeText(Cadastro.this,"Preencha todos os campos!",Toast.LENGTH_SHORT).show();
+                }else {
+                    EstacionamentoDAO esta = new EstacionamentoDAO(Cadastro.this);
+                    if (estacionamento == null) {
+                        estacionamento = new Estacionamento();
+                    }
+                    estacionamento.setNome(edtEstacionamento.getText().toString());
+                    esta.salvar(estacionamento);
+                    UsuarioDAO usud = new UsuarioDAO(Cadastro.this);
+                    if (usu == null) {
+                        usu = new Usuario();
+                    }
+                    if (edtSenha.getText().toString().equals(edtRepetirSenha.getText().toString())) {
+                        usu.setLogin(edtUser.getText().toString());
+                        usu.setSenha(edtSenha.getText().toString());
+                        usu.setEstacionamento(estacionamento);
+                        usud.salvar(usu);
+                    } else {
+                        Toast.makeText(Cadastro.this, "A senhas não correspondem!", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                estacionamento.setNome(edtEstacionamento.getText().toString());
-                esta.salvar(estacionamento);
-                UsuarioDAO usud = new UsuarioDAO(Cadastro.this);
-                if(usu == null){
-                    usu = new Usuario();
-                }
-                usu.setLogin(edtUser.getText().toString());
-                usu.setSenha(edtSenha.getText().toString());
-                usu.setEstacionamento(estacionamento);
-                usud.salvar(usu);
-                //nada
             }
         });
     }
